bb4713655daa8936d1cba0746ff8cecd7870adcd
meme <yongbok330915@gmail.com>
2026-09-15 15:21:46 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/travel-checklist/',
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: '/travel-checklist/', // ←これを追加
 })
+
